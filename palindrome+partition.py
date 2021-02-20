4e8f3f732dd7f7ef38377dbddb3d6d1c17f16b0d
Tao Yu <32440695+mintnsalmon@users.noreply.github.com>
2021-02-20 00:54:31 -0500

    Add files via upload

---
+++ palindrome+partition.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+class Solution(object):
+    def partition(self, s):
+        res = []
+        self.dfs(s, [], res)
+        return res
+    
+    def dfs(self, s, path, res):
+        if not s:
+            res.append(path)
+        for i in range(1, len(s)+1):
+            if self.isPalindrome(s[:i]):
+                self.dfs(s[i:], path+[s[:i]], res)
+                 
+    def isPalindrome(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1; r -= 1
+        return True
+
