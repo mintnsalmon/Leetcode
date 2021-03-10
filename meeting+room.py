316770aba14b24234035dec38bd234d0d383ea74
Tao Yu <32440695+mintnsalmon@users.noreply.github.com>
2021-03-09 21:37:38 -0500

    Add files via upload

---
+++ meeting+room.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+        return True
+
